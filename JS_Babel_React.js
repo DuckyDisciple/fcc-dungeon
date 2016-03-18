f65de0beb35615a821b386d2c544d5a6ee7218e3
Drew Sartorius <duckydesign@gmail.com>
2016-03-18 08:33:13 -0400

    Dungeon created

--- JS_Babel_React.js
+++ JS_Babel_React.js
@@ -1,3 +1,6 @@
+var dungeonWidth = 50;
+var dungeonHeight = 40;
+
 var Stats = React.createClass({
   render: function(){
     return (
@@ -13,20 +16,55 @@ var Stats = React.createClass({
 
 var Game = React.createClass({
   render: function(){
+    var board = this.props.dungeon;
+    var boardLayout = board.map(function(col){
+      return (
+        <div className="col">
+          {col.map(function(row){
+            if(row===0){
+              return <div className="square wall"></div>
+            }else if(row===1){
+              return <div className="square floor"></div>
+            }
+      })}
+        </div>
+      );
+    });
     return (
       <div className="game">
-        Game goes here
+        {boardLayout}
       </div>
     );
   }
 });
 
 var Container = React.createClass({
+  getInitialState: function(){
+    return {dungeon: [], mounted: false};
+  },
+  componentDidMount: function(){
+    this.setState({dungeon: this.generateDungeon(), mounted: true});
+  },
+  generateDungeon: function(){
+    var dungeon = [];
+    for(var i=0; i<dungeonWidth; i++){
+      var col = [];
+      for(var j=0; j<dungeonHeight; j++){
+        if(i===0 || i===dungeonWidth-1 || j===0 || j===dungeonHeight-1){
+          col.push(0);
+        }else{
+          col.push(1);
+        }
+      }
+      dungeon.push(col);
+    }
+    return dungeon;
+  },
   render: function(){
     return (
       <div className="container">
         <Stats />
-        <Game />
+        <Game dungeon={this.state.dungeon} />
       </div>
     );
   }
